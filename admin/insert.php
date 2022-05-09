76d7831be802510492ba0c53565f6fb5e8100499
davydebut <spanudavy@gmail.com>
2022-05-09 21:03:07 +0200

    burger code terminé

--- admin/insert.php
+++ admin/insert.php
@@ -1,5 +1,77 @@
 <?php
 require 'database.php';
+
+$nameError = $descriptionError = $priceError = $categoryError = $imageError = $name = $description = $price = $category = $image = "";
+
+if (!empty($_POST)) {
+    $name = checkInput($_POST['name']);
+    $description = checkInput($_POST['description']);
+    $price = checkInput($_POST['price']);
+    $category = checkInput($_POST['category']);
+    $image = checkInput($_FILES['image']['name']);
+    $imagePath = '../img/' . basename($image);
+    $imageExt = pathinfo($imagePath, PATHINFO_EXTENSION);
+    $isSuccess = true;
+    $isUploadSuccess = false;
+
+    if (empty($name)) {
+        $nameError = 'Ce champ ne peut pas être vide';
+        $isSuccess = false;
+    }
+    if (empty($description)) {
+        $descriptionError = 'Ce champ ne peut pas être vide';
+        $isSuccess = false;
+    }
+    if (empty($price)) {
+        $priceError = 'Ce champ ne peut pas être vide';
+        $isSuccess = false;
+    }
+    if (empty($category)) {
+        $categoryError = 'Ce champ ne peut pas être vide';
+        $isSuccess = false;
+    }
+    if (empty($image)) {
+        $imageError = 'Ce champ ne peut pas être vide';
+        $isSuccess = false;
+    } else {
+        $isUploadSuccess = true;
+        if ($imageExt != "jpg" && $imageExt != "png" && $imageExt != "jpeg" && $imageExt != "gif") {
+            $imageError = "Les fichiers autorises sont : .jpg .jpeg .png .gif";
+            $isUploadSuccess = false;
+        }
+        if (file_exists($imagePath)) {
+            $imageError = "Le fichier existe deja";
+            $isUploadSuccess = false;
+        }
+        if ($_FILES["image"]["size"] > 500000) {
+            $imageError = "Le fichier ne doit pas depasser les 500KB";
+            $isSuccess = false;
+        }
+        if ($isUploadSuccess) {
+            // il va prendre le chemin temporaire de l'image et le mettre dans le chemin de la variable $imagePath qui contient le dossier img
+            if (!move_uploaded_file($_FILES['image']["tmp_name"], $imagePath)) {
+                $imageError = "Il y a eu une erreur lors de l'upload";
+                $isUploadSuccess = false;
+            }
+        }
+    }
+    if ($isSuccess && $isUploadSuccess) {
+        $db = Database::connect();
+        $statement = $db->prepare("INSERT INTO items (name, description, price, category, image) VALUES (?, ?, ?, ?, ?)");
+        $statement->execute(array($name, $description, $price, $category, $image));
+        Database::disconnect();
+        header("Location: index.php");
+    }
+}
+
+function checkInput($data)
+{
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -24,23 +96,23 @@ require 'database.php';
             <h1><strong>Ajouter un item</strong></h1>
             <br>
             <form class="form" role="form" action="insert.php" method="POST" enctype="multipart/form-data">
-                <div class="form-group">
-                    <label for="name">Nom :</label>
+                <div>
+                    <label class="form-label" for="name">Nom :</label>
                     <input type="text" class="form-control" id="name" name="name" placeholder="Nom" value="<?php echo $name; ?>">
                     <span class="help-inline"><?php echo $nameError; ?></span>
                 </div>
-                <div class="form-group">
-                    <label for="description">Description :</label>
+                <div>
+                    <label class="form-label" for="description">Description :</label>
                     <input type="text" class="form-control" id="description" name="description" placeholder="Description" value="<?php echo $description; ?>">
                     <span class="help-inline"><?php echo $descriptionError; ?></span>
                 </div>
-                <div class="form-group">
-                    <label for="price">Prix : (en €)</label>
+                <div>
+                    <label class="form-label" for="price">Prix : (en €)</label>
                     <input type="number" step="0.01" class="form-control" id="price" name="price" placeholder="Prix" value="<?php echo $price; ?>">
                     <span class="help-inline"><?php echo $priceError; ?></span>
                 </div>
-                <div class="form-group">
-                    <label for="category">Categorie :</label>
+                <div>
+                    <label class="form-label" for="category">Categorie :</label>
                     <select class="form-control" name="category" id="category">
                         <?php
                         $db = Database::connect();
@@ -54,17 +126,17 @@ require 'database.php';
                     </select>
                     <span class="help-inline"><?php echo $categoryError; ?></span>
                 </div>
-                <div class="form-group">
-                    <label for="image">Séléctionner une image :</label>
+                <div>
+                    <label class="form-label" for="image">Séléctionner une image :</label>
                     <input type="file" name="image" id="image">
                     <span class="help-inline"><?php echo $imageError; ?></span>
                 </div>
+                <br>
+                <div class="form-actions">
+                    <button type="submit" class="btn btn-success"><span class="bi bi-pencil-fill"></span> Ajouter</button>
+                    <a href="index.php" class="btn btn-primary"><span class="bi-arrow-left"> Retour</span></a>
+                </div>
             </form>
-            <br>
-            <div class="form-actions">
-                <button type="submit" class="btn btn-success"><span class="bi bi-pencil-fill"></span> Ajouter</button>
-                <a href="index.php" class="btn btn-primary"><span class="bi-arrow-left"> Retour</span></a>
-            </div>
         </div>
     </div>
 </body>
